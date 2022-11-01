f87c282e9a1af24d44460128afc147edecfbffb3
Sime <sime.cvitan25@gmail.com>
2022-11-01 23:58:59 +0100

    init

---
+++ src/main/kotlin/com/example/wheretogo/WhereToGoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.wheretogo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhereToGoApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhereToGoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/wheretogo/WhereToGoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.wheretogo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhereToGoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
